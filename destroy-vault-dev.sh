6653e0d682494438dcd11cddeb484186dfa5b2da
tianyawy <tianya_wy@hotmail.com>
2017-05-15 22:54:58 +0800

    [victoryw] #3 fix the scriptt

--- destroy-vault-dev.sh
+++ destroy-vault-dev.sh
@@ -5,3 +5,5 @@ docker-compose down -v
 
 cd ../
 rm -rf vault-volume/file
+unset VAULT_ADDR
+unset VAULT_TOKEN
--- start-vault-dev.sh
+++ start-vault-dev.sh
@@ -2,3 +2,4 @@
 cd devops/
 docker-compose up -d
 export VAULT_ADDR=http://127.0.0.1:8201
+export VAULT_TOKEN=devroot
