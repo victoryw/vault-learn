a917c5d986595cb25db6d32f76d50223d687cc5c
tianyawy <tianya_wy@hotmail.com>
2017-05-20 04:39:39 +0800

    [victoryw] #n/a the dev migrate should self done

--- database-migration/migrate-db.sh
+++ database-migration/migrate-db.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 hostIp=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | sed -n '1p;1q'`
 export DBIP=$hostIp
+
+export VAULT_ADDR=http://127.0.0.1:8201
+export VAULT_TOKEN=devroot
+
 docker-compose up
 docker-compose down -v
 cd ../
