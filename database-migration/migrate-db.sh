2e79b7a2fd68629786d5de3a913c9cdce0ccf541
tianyawy <tianya_wy@hotmail.com>
2017-05-22 09:16:20 +0800

    [victoryw] #n/a use the dev vm

--- database-migration/migrate-db.sh
+++ database-migration/migrate-db.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-hostIp=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | sed -n '1p;1q'`
-export DBIP=$hostIp
+#hostIp=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | sed -n '1p;1q'`
+export DBIP=10.245.4.2
 
-export VAULT_ADDR=http://127.0.0.1:8241
 export VAULT_TOKEN=devroot
 
 docker-compose up
